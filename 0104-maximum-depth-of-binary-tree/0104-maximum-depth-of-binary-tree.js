18a6edf227facf64dd90506a0588cd0a671ab51a
Christian L. De La Torre <132640979+Knu09@users.noreply.github.com>
2024-07-05 22:07:00 +0800

    Time: 80 ms (5.35%), Space: 51.9 MB (36.60%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,6 +11,23 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-   if (!root) return 0
-   return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+      if (!root) return 0
+      // return 1 + Math.max(maxDepth(root.left), maxDepth(root.right)) */
+
+      let depth = 0
+      let queue = [root]
+
+      while (queue.length) {
+         let len = queue.length
+
+         for (let i = 0; i < len; i++) {
+            let current = queue.shift()
+
+            if(current.left) queue.push(current.left)
+            if(current.right) queue.push(current.right)
+         }
+         
+         depth++
+      }
+      return depth
 }
