11781698f4d19ad96f89b5ff191a389a6d34222f
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-08-06 14:23:37 +0800

    Time: 131 ms (5.97%), Space: 46.3 MB (6.95%)

---
+++ Java/0125-valid-palindrome/Solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+  public boolean isPalindrome(String s) {
+    String fixedStr = "";
+
+    // Check if the string is alphanumeric
+    for (char c : s.toCharArray()) {
+      if (Character.isDigit(c) || Character.isLetter(c)) {
+        fixedStr += c;
+      }
+    }
+    // convert fixed string to lowercase
+    fixedStr = fixedStr.toLowerCase();
+
+    // initialize sliding window
+    int first = 0;
+    int last = fixedStr.length() - 1;
+
+    // loop until first is equals to the last
+    while (first <= last) {
+
+      // checks if the first and last character is not identical
+      if (fixedStr.charAt(first) != fixedStr.charAt(last)) {
+        return false;
+      }
+
+      // increment first and last windows
+      first++;
+      last--;
+    }
+    return true;
+  }
+  public static void main(String[] args) {
+    Solution sol = new Solution();
+    System.out.print(sol.isPalindrome("amanaplanacanalpanama"));
+  }
+}
