2574b98cb1752a4a59698188d159fdd1ea355622
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-08-07 10:42:14 +0800

    Time: 5 ms (65.92%), Space: 43.79 MB (95.79%)

---
+++ Java/0009_palindrome_number/Solution_02.java
@@ -0,0 +1,31 @@
+public class Solution_02 {
+  public boolean isPalindromeNum(int x) {
+    if (x == 0) {
+      return true;
+    }
+    if (x < 0 || x % 10 == 0) {
+      return false;
+    }
+
+    int original = x;
+    int reversed_int = 0;
+
+    // Reversed integer builder
+    while (x != 0) {
+      // Get the last digit of x and store in remainder
+      int remainder = x % 10;
+
+      // Removes the last digit of x
+      x /= 10;
+
+      // Build the reversed int by mutiplying the current and add the remainder
+      reversed_int = reversed_int * 10 + remainder;
+    }
+    return original == reversed_int;
+  }
+
+  public static void main(String[] args) {
+    Solution_02 sol = new Solution_02();
+    System.out.println(sol.isPalindromeNum(12121));
+  }
+}
