27c53135f7c970128c6f36b88580e9369ac0cebb
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-08-14 23:54:50 +0800

    Time: 4 ms (99.59%), Space: 45.63 MB (24.41%)

---
+++ Java/0680_valid_palindrome_ii/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+  public boolean validPalindrome(String s) {
+    int left = 0, right = s.length() - 1;
+    while (left < right) {
+      if (s.charAt(left) != s.charAt(right)) {
+        return (isPalindrome(s, left + 1, right) ||
+                isPalindrome(s, left, right - 1));
+      }
+      left++;
+      right--;
+    }
+    return true;
+  }
+
+  private boolean isPalindrome(String str, int left, int right) {
+    while (left < right) {
+      if (str.charAt(left) != str.charAt(right)) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    Solution sol = new Solution();
+    System.out.println(sol.validPalindrome("aba"));
+  }
+}
